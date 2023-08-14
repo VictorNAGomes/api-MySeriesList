13ffd627079a94d5aabcffe421b7babce2ece919
VictorNAGomes <dev.victorgomes@gmail.com>
2023-08-14 18:27:36 -0300

    feat: config lint-staged

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-﻿    const a = 2
-const b = 2;
+﻿const a = 2
+const b = 2
 
-console.error('b' + b)
-console.error('a' + a)
+console.log('b' + b)
+    console.error('a' + a)
 
 if (a === 2 && a === 2 && a === 2 && a === 2 && a === 2) {
-    console.error('a')
+    console.error('aa');
 }
