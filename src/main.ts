f7727a854a5c7d20f86215164fb024808c221f78
VictorNAGomes <dev.victorgomes@gmail.com>
2023-08-14 22:04:26 -0300

    feat: add husky config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 ﻿const a = 2
 const b = 2
 
-console.log('b' + b)
-    console.error('a' + a)
+console.info('b' + b)
+console.info('a' + a)
 
 if (a === 2 && a === 2 && a === 2 && a === 2 && a === 2) {
-    console.error('aa');
+    console.info('aaa')
 }
